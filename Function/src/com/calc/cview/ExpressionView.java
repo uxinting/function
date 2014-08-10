32b49912b381fc41dc4bf3625a9b529073f61e6e
xinting <xinting@xinting-PC>
2014-08-10 10:08:49 +0800

    MultiAutoTextView

--- Function/src/com/calc/cview/ExpressionView.java
+++ Function/src/com/calc/cview/ExpressionView.java
@@ -5,8 +5,9 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
 
-public class ExpressionView extends AutoCompleteTextView {
+public class ExpressionView extends MultiAutoCompleteTextView {
 	
 	private String[]	tips = null;
 	private ArrayAdapter<String> adapter = null;
@@ -22,6 +23,7 @@ public class ExpressionView extends AutoCompleteTextView {
 	public ExpressionView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		
+		setTokenizer(new OperatorTokenizer());
 	}
 
 	public String[] getTips() {
@@ -39,6 +41,47 @@ public class ExpressionView extends AutoCompleteTextView {
 		}
 		
 		setAdapter(adapter);
+	}
+
+	public class OperatorTokenizer implements MultiAutoCompleteTextView.Tokenizer {
+		
+		private final char[] tokens = { '+', '-', '*', '/' };
+
+		private boolean isToken(char c) {
+			for ( char tc : tokens ) {
+				if ( tc == c ) return true;
+			}
+			return false;
+		}
+		
+		@Override
+		public int findTokenStart(CharSequence text, int cursor) {
+			
+			int i = cursor;
+			
+			while ( i > 0 && !isToken(text.charAt(i-1)) ) i--;
+			
+			return i;
+		}
+
+		@Override
+		public int findTokenEnd(CharSequence text, int cursor) {
+			
+			int i = cursor;
+			
+			while ( i < text.length() ) {
+				if ( isToken(text.charAt(i)) ) break;
+				else i++;
+			}
+			
+			return i;
+		}
+
+		@Override
+		public CharSequence terminateToken(CharSequence text) {
+			return text;
+		}
+		
 	}
 
 }
